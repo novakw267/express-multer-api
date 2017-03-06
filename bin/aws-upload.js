1afb5a77fb6ff56f8cabe2952698cb936fc44fe6
novakw267 <novakw267@gmail.com>
2017-03-06 15:08:36 -0500

    Refactors bin script into lib/ module
    Moved most of the code from bin/aws-upload that was responsible for
    the s3 upload piece to it's own file in the lib directory. I saw the
    need to using this code in more than one place, so the lib/s3-upload
    module can now be used from more than one place in our application.
    The bin.aws-upload script still works (and just implements the lib one)

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -1,16 +1,8 @@
 'use strict';
 
 require('dotenv').load();
-const AWS = require('aws-sdk');
-// requiring the amazon storage
-const s3 = new AWS.S3();
-const fs = require('fs');
-// requiring the files to use mime which we installed via npm
-// it will allow things we upload to the amazing server to figure out
-// the file type
-const mime = require('mime');
-const path = require('path');
-const crypto = require('crypto');
+
+const s3Upload = require('../lib/s3-upload');
 
 let file = {
   // the path of the file we're trying to stream
@@ -18,62 +10,6 @@ let file = {
   title: process.argv[3]
 };
 
-// This will find out what type of file we uploaded
-let mimeType = mime.lookup(file.path);
-// and get rid of the extention
-let ext = path.extname(file.path);
-
-// command for setting up the stream
-let stream = fs.createReadStream(file.path);
-// setting new Date( with no params gives todays date)
-// this puts that into an array with the time
-// the .split puts forces only the first part to return
-let folder = (new Date()).toISOString().split('T')[0];
-
-// this decided whether or a promise succeds or gets rejected.
-new Promise((resolve, reject) => {
-  // we use randomBytes to get our file name
-    crypto.randomBytes(16, (error, buffer) => {
-      if (error) {
-        // if error the promoise gets rejected and log the error
-        reject(error);
-      } else {
-        console.log("buffer is ", buffer);
-        console.log("buffer.toS is ", buffer.toString('hex'));
-        resolve(buffer.toString('hex'));
-      }
-    });
-  })
-  // this is why we call the params filename becuase thats what it is
-  .then((filename) => {
-    // filename is what the promise returns
-    // if there is an error nothing happens here
-    let params = {
-      ACL: 'public-read',
-      ContentType: mimeType,
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: `${folder}/${filename}${ext}`,
-      Body: stream
-    };
-
-    // calling the params to upload to our bucket,
-    //  the function next to params is a call back
-    // this allows the return or the rejection of the new promise
-    return new Promise((resolve, reject) => {
-      // promisifying this allows us to return a promise with the
-      // resolved data
-      s3.upload(params, function(error, data) {
-        if (error) {
-          // console.log(error);
-          reject(error);
-        } else {
-          // console.log(data);
-          resolve(data);
-        }
-      });
-    });
-
-  })
-  .then(console.log)
-  .catch(console.error);
-//
+s3Upload(file)
+.then(console.log)
+.catch(console.error);
