a7754ab61ac64964e8b2f74f8a4766d15708255b
novakw267 <novakw267@gmail.com>
2017-03-06 14:17:56 -0500

    fixes filename and folder problem
    Previously files we just getting saved in the root director of our
    bucket. Solved this by adding today's date as a folder and prepending
    our filename with that. The next thing we did via
    crypto.randomBytes is create a random string to save out files as
    on AWS. The last thing we did was promisify both the s3.upload and
    crypto.randomBytes so both implement promises that are resolved or
    rejected based on whether or not they work.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -10,6 +10,7 @@ const fs = require('fs');
 // the file type
 const mime = require('mime');
 const path = require('path');
+const crypto = require('crypto');
 
 let file = {
   // the path of the file we're trying to stream
@@ -24,19 +25,55 @@ let ext = path.extname(file.path);
 
 // command for setting up the stream
 let stream = fs.createReadStream(file.path);
+// setting new Date( with no params gives todays date)
+// this puts that into an array with the time
+// the .split puts forces only the first part to return
+let folder = (new Date()).toISOString().split('T')[0];
 
-// the params that we need to include for upload.
-let params = {
-  ACL: 'public-read',
-  ContentType: mimeType,
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: `${file.title}${ext}`,
-  Body: stream
-};
+// this decided whether or a promise succeds or gets rejected.
+new Promise((resolve, reject) => {
+  // we use randomBytes to get our file name
+    crypto.randomBytes(16, (error, buffer) => {
+      if (error) {
+        // if error the promoise gets rejected and log the error
+        reject(error);
+      } else {
+        console.log("buffer is ", buffer);
+        console.log("buffer.toS is ", buffer.toString('hex'));
+        resolve(buffer.toString('hex'));
+      }
+    });
+  })
+  // this is why we call the params filename becuase thats what it is
+  .then((filename) => {
+    // filename is what the promise returns
+    // if there is an error nothing happens here
+    let params = {
+      ACL: 'public-read',
+      ContentType: mimeType,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: `${folder}/${filename}${ext}`,
+      Body: stream
+    };
 
-// calling the params to upload to our bucket,
-//  the function next to params is a call back
+    // calling the params to upload to our bucket,
+    //  the function next to params is a call back
+    // this allows the return or the rejection of the new promise
+    return new Promise((resolve, reject) => {
+      // promisifying this allows us to return a promise with the
+      // resolved data
+      s3.upload(params, function(error, data) {
+        if (error) {
+          // console.log(error);
+          reject(error);
+        } else {
+          // console.log(data);
+          resolve(data);
+        }
+      });
+    });
+
+  })
+  .then(console.log)
+  .catch(console.error);
 //
-s3.upload(params, function(err, data) {
-  console.log(err, data);
-});
