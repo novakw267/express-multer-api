f333b56eb099ee32b922968690fbf67f3d933199
novakw267 <novakw267@gmail.com>
2017-03-06 15:56:19 -0500

    Fixes mongoose middleware broken path
    Config/database was incorrectly pathed and needed a ../.. in front
    of it to access the correct directory.
    Adds upload mongoose model requiring title and url as attributes
    Implements this model in the bin/script so we're testing everything
    from the command line -> AWS -> mongo which is all working correctly.
    Removed example model because we don't need it anymore.

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const database = require('config/database');
+const database = require('../../config/database');
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI || database.mongoose.uri;
 mongoose.Promise = global.Promise;
--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -3,6 +3,10 @@
 require('dotenv').load();
 
 const s3Upload = require('../lib/s3-upload');
+const Upload = require('../app/models/upload');
+const mongoose = require('../app/middleware/mongoose');
+
+// Upload.create(title, url);
 
 let file = {
   // the path of the file we're trying to stream
@@ -10,6 +14,16 @@ let file = {
   title: process.argv[3]
 };
 
+// returns a promise, and some data.
 s3Upload(file)
+.then((s3Response) => {
+  // get the url
+  let url = s3Response.Location;
+  return Upload.create({
+    title: file.title,
+    url: url
+  });
+})
 .then(console.log)
-.catch(console.error);
+.catch(console.error)
+.then(() => mongoose.connection.close());
